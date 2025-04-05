636719b5e3c603008dbfc9b67927cb48c5f828b8
RachnaRamesh <40124528+RachnaBhatade@users.noreply.github.com>
2025-04-05 17:18:03 +0530

    Added the furniture website files

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var about = document.getElementsByClassName(buttonscls);
+  for (var i = 0; i < about.length; i++) {
+    about[i] = about[i].sty;
+  }
+});
