d244c1a8273613955e70892343a59f40cabc2576
rahulshinde095 <rahulshinde095@gmail.com>
2025-11-18 15:39:17 +0530

    Major Changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var about = document.getElementsByClassName(buttonscls);
+  var about = document.getElementsByClassName("navbarCls"); // Fixed class name reference
   for (var i = 0; i < about.length; i++) {
-    about[i] = about[i].sty;
+    about[i].style.cursor = "pointer"; // Corrected property access and added functionality
   }
 });
